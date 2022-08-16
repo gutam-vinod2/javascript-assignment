90b16696b8ddc6d06b51ce6682acc168e52882dc
[firstname lastname] <[valid-email]>
2022-08-16 20:01:03 +0530

    assign1

---
+++ ass1.js
@@ -0,0 +1,32 @@
+var x=5;
+var y=10;
+var  z = (x=y);
+console.log(z)
+
+
+var x=5;
+var y=10;
+var z = (y +=1)
+console.log(z)
+
+
+var x=5;
+var y=10;
+var  z = (y-= 1);
+console.log(z)
+
+
+var x=5;
+var y=10;
+var  z = (y*= 5);
+console.log(z)
+
+
+var x=5;
+var y=10;
+var  z = (y/= 5);
+console.log(z)
+
+
+
+
